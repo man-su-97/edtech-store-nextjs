de820359abedf498f573e893917575dd3acb1259
man-su-97 <sumanmandal15135@gmail.com>
2024-05-15 13:12:20 +0530

    title changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h2 className="text-2xl text-center">Chai Aur Code</h2>
+      <h2 className="text-2xl text-center">Music Academy</h2>
     </main>
   );
 }
