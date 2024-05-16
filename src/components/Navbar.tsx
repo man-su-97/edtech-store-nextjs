a1d364831b0cb26a9cd0f7230df21e0a4feb98c1
man-su-97 <sumanmandal15135@gmail.com>
2024-05-16 20:47:31 +0530

    hero section done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <Link href={"/home"}>
+        <Link href={"/"}>
           <MenuItem
             setActive={setActive}
             active={active}
